25230fabb3b58b8a9b9167ba5c18ffc4e3ed15ca
Prashant Singh <prasingh26@publicisgroupe.net>
2020-02-06 14:20:36 +0530

    Initial Commit for project setup

---
+++ src/main/java/com/prashant/inventory/service/InventoryLaoderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.inventory.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryLaoderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryLaoderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prashant/inventory/service/InventoryLaoderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.inventory.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryLaoderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
